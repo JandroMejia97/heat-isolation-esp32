20f5dcf7aa28d0f9102d0f944f10f0facd65910e
Gabriel Torres <111906750+gabitorres2@users.noreply.github.com>
2023-11-08 10:29:39 -0300

    http-server mode STA/AP

--- src/STA.cpp
+++ src/STA.cpp
@@ -1,6 +1,7 @@
  #include <WiFi.h>
  #include <Adafruit_MLX90614.h>
-
+ #include <Arduino.h>
+ #include <SPI.h>
 
 //------------------Servidor Web en puerto 80---------------------
 
@@ -8,8 +9,8 @@ WiFiServer server(80);
 
 //---------------------Credenciales de WiFi-----------------------
 
-const char* ssid     = "NADIA";
-const char* password = "40393705";
+const char* ssid     = "Fibertel WiFi839 2.4Ghz";
+const char* password = "R14G4NLOST97";
 
 //---------------------VARIABLES GLOBALES-------------------------
 int contconexion = 0;
@@ -19,58 +20,58 @@ String header; // Variable para guardar el HTTP request
 String estadoSalida = "off";
 
 const int salida = 2;
+uint8_t leer = 0;
 
 //------------------------CODIGO HTML------------------------------
-String pagina = "<!DOCTYPE html>"
-"<html>"
-"<head>"
-"<meta charset='utf-8' />"
-"<meta name='viewport' content='width=device-width, initial-scale=1'>"
-"<title>Servidor Web ESP32</title>"
-"<style>"
-"body {font-family: Arial, sans-serif; background-color: #f0f0f0; display: flex; flex-direction: column; align-items: center; justify-content: center; height: 100vh; margin: 0; padding: 0;}"
-"h1 {color: #000000;}"
-"h2 {color: #50a8a0; font-size: 14px;}"
-"table {width: 100%;}"
-"td {text-align: center;}"
-"button.on {background-color: #4CAF50; border: none; color: white; padding: 15px 32px;"
-"text-decoration: none; display: inline-block; font-size: 16px; margin: auto; cursor: pointer; border-radius: 12px;}"
-"button.off {background-color: #f44336; border: none; color: white; padding: 15px 32px;"
-"text-decoration: none; display: inline-block; font-size: 16px; margin: auto; cursor: pointer; border-radius: 12px;}"
-"</style>"
-"</head>"
-"<body>"
-"<h1>Servidor Web ESP32</h1>"
-"<h2>Control de Alimentación</h2>"
-"<table>"
-"<tr><td><a href='/on'><button class='on'>ON</button></a></td></tr>"
-"<tr><td><a href='/off'><button class='off'>OFF</button></a></td></tr>"
-"</table>"
-"<h2>Control DHT11 Interno</h2>"
-"<table>"
-"<tr><td><a href='/dht11Inton'><button class='on'>DHT11 ON</button></a></td></tr>"
-"<tr><td><a href='/dht11Intoff'><button class='off'>DHT11 OFF</button></a></td></tr>"
-"</table>"
-"<h2>Control DHT11 Externo</h2>"
-"<table>"
-"<tr><td><a href='/dht11Exton'><button class='on'>DHT11 ON</button></a></td></tr>"
-"<tr><td><a href='/dht11Extoff'><button class='off'>DHT11 OFF</button></a></td></tr>"
-"</table>"
-"<h2>Control de Sensor Infrarrojo</h2>"
-"<table>"
-"<tr><td><a href='/SIon'><button class='on'>ON</button></a></td></tr>"
-"<tr><td><a href='/SIoff'><button class='off'>OFF</button></a></td></tr>"
-"</table>"
-"</body>"
-"</html>";
 
+String htmlHeader = "<!DOCTYPE html>"
+    "<html>"
+    "<head>"
+    "<meta charset='utf-8' />"
+    "<meta name='viewport' content='width=device-width, initial-scale=1'>"
+    "<style>"
+    "body {font-family: Arial, sans-serif; background-color: #f0f0f0; display: flex; flex-direction: column; align-items: center; justify-content: center; height: 100vh; margin: 0; padding: 0;}"
+    "h1 {color: #000000;}"
+    "h2 {color: #50a8a0; font-size: 14px;}"
+    "table {width: 100%;}"
+    "td {text-align: center;}"
+    "button.on {background-color: #4CAF50; border: none; color: white; padding: 15px 32px;"
+    "text-decoration: none; display: inline-block; font-size: 16px; margin: auto; cursor: pointer; border-radius: 12px;}"
+    "button.off {background-color: #f44336; border: none; color: white; padding: 15px 32px;"
+    "text-decoration: none; display: inline-block; font-size: 16px; margin: auto; cursor: pointer; border-radius: 12px;display:none}"
+    "</style>"
+    "</head>";
+
+String on_html = 
+    "<body>"
+    "<h1>Servidor Web ESP32</h1>"
+    "<h3>Control de Temperatura en Entorno Aislado</h3>"
+    "<h2>Ingrese la temperatura maxima deseada en grados centrigados</h2>"
+    "<table>"
+    "<tr><td><input type='text' id='tempMax' name='tempMax' placeholder='Temperatura Máxima'></td></tr>"
+    "<tr><td><a href='/on'><button class='on'>ON</button></a></td></tr>"
+    "</table>"
+    "</body>"
+    "</html>";
+
+
+String off_html = 
+    "<body>"
+    "<h1>Servidor Web ESP32</h1>"
+    "<h2>Control de Temperatura en Entorno Aislado</h2>"
+    "<tr><td><a href='/off'><button class='off'>OFF</button></a></td></tr>"
+    "</table>"
+    "</body>"
+    "</html>";
 
 //---------------------------SETUP--------------------------------
 void setup() {
   Serial.begin(115200);
+  
   Serial.println("");
   
   pinMode(salida, OUTPUT); 
+  
   digitalWrite(salida, LOW);
 
   // Conexión WIFI
@@ -121,15 +122,13 @@ void loop(){
               Serial.println("GPIO on");
               estadoSalida = "on";
               digitalWrite(salida, HIGH);
+              client.println(htmlHeader.concat(on_html));
             } else if (header.indexOf("GET /off") >= 0) {
               Serial.println("GPIO off");
               estadoSalida = "off";
               digitalWrite(salida, LOW);
+              client.println(htmlHeader+off_html);
             }
-            
-            // Muestra la página web
-            client.println(pagina);
-            
             // la respuesta HTTP temina con una linea en blanco
             client.println();
             break;
@@ -141,6 +140,10 @@ void loop(){
         }
       }
     }
+
+    // Muestra la página web 
+    client.println(header.concat(on_html));
+
     // Limpiamos la variable header
     header = "";
     // Cerramos la conexión
